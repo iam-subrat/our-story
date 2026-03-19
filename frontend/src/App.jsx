ed52faa675cb5e39fb2b68e964ac81132b123842
Justin Truman <37294184+su-brat@users.noreply.github.com>
2026-03-20 00:57:09 +0530

    routing fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import UserTimeline from './pages/UserTimeline'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Landing />} />
